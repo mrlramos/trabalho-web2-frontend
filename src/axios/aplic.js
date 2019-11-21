24620872051324849d0470fe070cfdf293f78098
Marlon Ramos <marlon_raoni10@hotmail.com>
2019-11-21 03:19:40 -0300

    Terminando trabalhoweb2

---
+++ src/axios/aplic.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+const aplic = axios.create({ baseURL: 'http://35.225.147.97/' });
+
+export default aplic;
---
+++ src/paginas/principal/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import "./style.css"
+import aplic from '../../axios/aplic';
+
+export default class Main extends Component {
+    state = {
+        produtos: [],
+        mesagem:[]
+    }
+    componentDidMount() {
+        this.loadProducts();
+    }
+    loadProducts = async () => {
+       
+        var response = await aplic.get('/');
+
+        this.setState({ produtos: response.data[0] });
+    }
+    render() {
+        return (
+            <div className='product-list'>
+                <div className="row">
+                    {this.state.produtos.map(produto => (
+                        <div className="col-sm-6 col-md-4" key={produto._id}>
+                            <div className="thumbnail" >
+                                <img src={produto.imagePath} alt='' />
+                                <div className="caption">
+                                    <h3>{produto.title}</h3>
+                                    <p>{produto.description}</p>
+                                    <p>R${produto.price},00</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        )
+    }
+    
+}
