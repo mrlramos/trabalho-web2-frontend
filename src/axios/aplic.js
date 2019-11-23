cb0e7418e01af295a0a67b971935714a5feae291
mrlramos <31023480+mrlramos@users.noreply.github.com>
2019-11-23 00:44:06 -0300

    Update aplic.js

--- src/axios/aplic.js
+++ src/axios/aplic.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const aplic = axios.create({ baseURL: 'http://35.225.147.97/' });
+const aplic = axios.create({ baseURL: 'http://35.222.131.126/' });
 
 export default aplic;
